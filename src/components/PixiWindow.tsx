128f7e3b5f413b33cea0075da57dc3b8e8179bd5
Hoshi Ran <ureshinostyle350@gmail.com>
2021-12-04 11:57:21 +0900

    [add] PixiJS , ReactPixi

---
+++ src/components/PixiWindow.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Stage, AnimatedSprite, Container } from "@inlet/react-pixi";
+
+const PixiWindow: FC = () => {
+  return <Stage>{/* ここに Pixi Component */}</Stage>;
+};
+
+export default PixiWindow;
