4288933380c87d85fdc4f4c5321854a7bf4723b5
Tomoch0104 <to0104mo2517@icloud.com>
2021-12-06 20:44:55 +0900

    コメントアウト

--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -1,1 +1,5 @@
 export { default as Label } from "./Label";
+export { default as CommonInput } from "./CommonInput";
+export { default as PrimaryButton } from "./PrimaryButton";
+export { default as DropdownIcon } from "./DropdownIcon";
+export { default as Button } from "./Button";
