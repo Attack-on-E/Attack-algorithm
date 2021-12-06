a034f4d85ad3d17d7ff494b8c9345cd4dcd72ea3
YukiSuga10 <sugamon121028@gmail.com>
2021-12-06 20:09:34 +0900

    [fix]Button

--- src/components/atoms/primaryButton.tsx
+++ src/components/atoms/primaryButton.tsx
@@ -9,7 +9,7 @@ type PROPS = {
 
 const PrimaryButton: React.FC<PROPS> = (props) => {
   return (
-    <button className="text-center text-lg text-white w-80 py-5 border-2 border-red-600 bg-primary bg-opacity-62 font-semibold rounded-full hover:bg-onPrimary">
+    <button className="text-center text-3xl text-white w-80 py-5 shadow-2xl border-4 border-red-600 bg-primary bg-opacity-62  font-semibold rounded-full hover:bg-onPrimary">
       <Link href={props.href}>{props.buttonText}</Link>
     </button>
   );
